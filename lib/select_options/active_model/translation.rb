c43ffe7c8a11503bf417241a50ba653120b2b2f6
Dmitriy Vorotilin <mydeeptown@gmail.com>
2011-10-26 12:50:59 +0400

    return empty string when value is empty

--- lib/select_options/active_model/translation.rb
+++ lib/select_options/active_model/translation.rb
@@ -1,11 +1,10 @@
 module ActiveModel
   module Translation
     # Transforms composite attribute names into a more human format
-    #
-    #   Person.human_composite_attribute_name("role", "admin") # => "Administrator"
-    #
+    #  Person.human_composite_attribute_name("role", "admin") # => "Administrator"
     # Specify +options+ with additional translating options.
     def human_composite_attribute_name(attribute, value, options = {})
+      return "" unless value.present?
       default = :"#{i18n_scope}.composite_attributes.#{model_name.i18n_key}.#{attribute}.#{value}"
       human_attribute_name "#{attribute}_#{value}", options.reverse_merge(:default => default)
     end
